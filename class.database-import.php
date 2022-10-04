ab898bef3df6f1ee6a65c6fa411e0e7d6d246251
Vladislav <vladprofmet@gmail.com>
2022-10-04 08:54:10 +0300

    Update class.database-import.php

--- class.database-import.php
+++ class.database-import.php
@@ -7,6 +7,11 @@ class DataBaseImport{
 
     private   $seconddb;
     public $table = '';
+//    private string $HOST = "nixwood2.mysql.tools";
+//    private string $USER = "nixwood2_crimea1";
+//    private string $PASS = "y^8E5a+y8C";
+//    private string $DATABASE = "nixwood2_crimea1";
+
     private string $HOST = "localhost";
     private string $USER = "root";
     private string $PASS = "";
@@ -108,9 +113,12 @@ class DataBaseImport{
 
     public function dbSelect()
     {
-        return $this->getDb()->get_results("SELECT * FROM `$this->table`");
-    }
 
+        return $this->getDb()->get_results(
+                "SELECT * FROM `$this->table` 
+                ORDER BY `$this->table`.`date` 
+                ASC LIMIT 100");
+    }
 
     private function insertPost()
     {
@@ -123,12 +131,12 @@ class DataBaseImport{
             ];
 
             $data = [
-                    'post_title' => $publication->title_uk,
-                    'post_date' => $publication->date,
-                    'post_content' => $publication->content_uk,
-                    'post_excerpt' => $publication->lead_uk,
-                    'post_status' =>$active[$publication->active_uk],
-                    'post_name'=>$publication->id.'-'.$publication->uri_uk
+                'post_title' => $publication->title_ru,
+                'post_date' => $publication->date,
+                'post_content' => $publication->content_ru,
+                'post_excerpt' => $publication->lead_ru,
+                'post_status' =>$active[$publication->active_ru],
+                'post_name'=>$publication->id.'-'.$publication->uri_ru
             ];
 
             $post_id = wp_insert_post($data);
@@ -137,47 +145,60 @@ class DataBaseImport{
 
             $this->saveThumbnail($image,$post_id);
 
-            $dataRu = [
-                'post_title' => $publication->title_ru,
-                'post_date' => $publication->date,
-                'post_content' => $publication->content_ru,
-                'post_excerpt' => $publication->lead_ru,
-                'post_status' =>$active[$publication->active_ru],
-                'post_name'=>$publication->id.'-'.$publication->uri_ru
+            if ($publication->active_uk == 1){
+                $dataUk = [
+                    'post_title' => $publication->title_uk,
+                    'post_date' => $publication->date,
+                    'post_content' => $publication->content_uk,
+                    'post_excerpt' => $publication->lead_uk,
+                    'post_status' =>$active[$publication->active_uk],
+                    'post_name'=>$publication->id.'-'.$publication->uri_uk
+
+                ];
+
+                $post_id_ru = $this->saveTranslationPost($dataUk,$post_id,'ru');
+                $this->saveThumbnail($image,$post_id_ru);
+
+            }
+            if ($publication->active_en == 1) {
+
+                $dataEn = [
+                    'post_title' => $publication->title_en,
+                    'post_date' => $publication->date,
+                    'post_content' => $publication->content_en,
+                    'post_excerpt' => $publication->lead_en,
+                    'post_status' => $active[$publication->active_en],
+                    'post_name' => $publication->id . '-' . $publication->uri_en
+
+                ];
+
+                $post_id_en = $this->saveTranslationPost($dataEn, $post_id, 'en');
+
+                $this->saveThumbnail($image,$post_id_en);
+
+            }
+            if ($publication->active_qt == 1) {
+
+                $dataQt = [
+                    'post_title' => $publication->title_qt,
+                    'post_date' => $publication->date,
+                    'post_content' => $publication->content_qt,
+                    'post_excerpt' => $publication->lead_qt,
+                    'post_status' => $active[$publication->active_qt],
+                    'post_name' => $publication->id . '-' . $publication->uri_qt
+                ];
+
+                $post_id_qt = $this->saveTranslationPost($dataQt, $post_id, 'qt');
+
+                $this->saveThumbnail($image,$post_id_qt);
 
 
-            ];
-
-            $this->saveTranslationPost($dataRu,$post_id,'ru');
-
-            $dataEn =[
-                'post_title' => $publication->title_en,
-                'post_date' => $publication->date,
-                'post_content' => $publication->content_en,
-                'post_excerpt' => $publication->lead_en,
-                'post_status' =>$active[$publication->active_en],
-                'post_name'=>$publication->id.'-'.$publication->uri_en
-
-            ];
-
-            $this->saveTranslationPost($dataEn,$post_id,'en');
-
-            $dataQt =  [
-                'post_title' => $publication->title_qt,
-                'post_date' => $publication->date,
-                'post_content' => $publication->content_qt,
-                'post_excerpt' => $publication->lead_qt,
-                'post_status' =>$active[$publication->active_qt],
-                'post_name'=>$publication->id.'-'.$publication->uri_qt
-            ];
-
-            $this->saveTranslationPost($dataQt,$post_id,'qt');
+            }
 
         }
 
         return true;
     }
-
 
     public function saveTranslationPost($data,$post_id,$code)
     {
@@ -200,27 +221,45 @@ class DataBaseImport{
 
         do_action( 'wpml_set_element_language_details', $set_language_args );
 
+        return $translation_id;
     }
-
-
-
-
 
     public  function saveThumbnail($image,$post_id){
 
 
-        $filename = '/'.$image[0]->path.'/'.$image[0]->name;
+       $filename = 'D:/OpenServer/domains/crimea.loc/wp-content/uploads/uploads/'.$image[0]->name_system.'.'.$image[0]->extension;
 
+
+
+
+//        $filename = '/home/nixwood2/nixwood.com.ua/crimea/wp-content/uploads/2022/10/0a3dfd746d4b1e1bda05a23eee7fbe89.jpg';
+
+        $filetype = wp_check_filetype( basename( $filename ), null );
+
+        var_dump(wp_upload_dir());
+
+        $wp_upload_dir = wp_upload_dir();
 
         $attachment = array(
-            'guid'           => $filename,
-            'post_mime_type' => $image[0]->extension,
-            'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+            'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
+            'post_mime_type' => $filetype['type'],
+            'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $image[0]->name_system.'.'.$image[0]->extension ) ),
             'post_content'   => '',
             'post_status'    => 'inherit'
         );
 
         $attach_id = wp_insert_attachment( $attachment, $filename, $post_id );
+
+        // Connect the desired file, if it is not already connected
+        // wp_generate_attachment_metadata() depends on this file.
+                require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+        // Create metadata for the attachment and update the post in the database.
+                $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+                wp_update_attachment_metadata( $attach_id, $attach_data );
+
+                set_post_thumbnail( $post_id, $attach_id );  // set post thumnail (featured image) for the given post
+
 
 
     }
